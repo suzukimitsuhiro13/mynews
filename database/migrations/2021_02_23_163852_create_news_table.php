5227e4f379104ecf794ebe55425112b4b52958cc
名前 <ec2-user@ip-172-31-7-88.us-east-2.compute.internal>
2021-02-24 02:52:36 +0000

    de-ta

--- database/migrations/2021_02_23_163852_create_news_table.php
+++ database/migrations/2021_02_23_163852_create_news_table.php
@@ -15,9 +15,6 @@ class CreateNewsTable extends Migration
     {
         Schema::create('news', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title'); // ニュースのタイトルを保存するカラム
-            $table->string('body');  // ニュースの本文を保存するカラム
-            $table->string('image_path')->nullable();  // 画像のパスを保存するカラム
             $table->timestamps();
         });
     }
